7ce1323031c0e80a567c04aecbdd97b7a243defc
jcsf2020 <jcsf2020@users.noreply.github.com>
2025-08-25 22:40:03 +0100

    chore(og): bump version param para forçar recache social

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export const metadata: Metadata = {
     type: 'website',
     images: [
       {
-        url: 'https://crsetsolutions.com/images/social-share-1200x630.jpg',
+        url: 'https://crsetsolutions.com/images/social-share-1200x630.jpg?v=202508252240',
         width: 1200,
         height: 630,
         alt: 'CRSET Solutions - Boris, Laya e Irina',
@@ -46,7 +46,7 @@ export const metadata: Metadata = {
     card: 'summary_large_image',
     title: 'CRSET Solutions - Soluções Digitais Inteligentes',
     description: 'Transforme o seu negócio com tecnologia inteligente',
-    images: ['https://crsetsolutions.com/images/social-share-1200x630.jpg'],
+    images: ['https://crsetsolutions.com/images/social-share-1200x630.jpg?v=202508252240'],
   },
   robots: {
     index: true,
