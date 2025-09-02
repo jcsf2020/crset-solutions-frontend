2e6b45e4bbd5e92d28a7c13b08bd2a48603a99fd
jcsf2020 <jcsf2020@users.noreply.github.com>
2025-09-02 22:02:37 +0100

    feat(assets): usar Boris definitivo (boris_variacao_1) no Hero

--- src/components/MascoteBoris.tsx
+++ src/components/MascoteBoris.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function MascoteBoris() {
   return (
     <Image
-      src="/mascotes/Boris.png"
+      src="/mascotes/boris.png"
       alt="Mascote Boris"
       width={300}
       height={300}
